07affacc4345eb6e84060cc16e159edae8317f47
szczyglis <szczyglis83@gmail.com>
2013-10-11 15:07:58 +0200

    Version file updated

--- web/version.php
+++ web/version.php
@@ -7,7 +7,7 @@
 	(c) 2013 Marcin Szczyglinski
 	szczyglis83@gmail.com
 	GitHUB: https://github.com/phpos/
-	File version: 1.0.0, 2013.10.10
+	File version: 1.0.0, 2013.10.11
  
 **********************************
 */
@@ -15,9 +15,9 @@
 
 
 /* **************************************** */
-	define('PHPOS_VERSION', '1.0');
-	define('PHPOS_BUILD', '2013.10.10');	
-	define('PHPOS_VERSION_NAME', '1.0.0 beta');
+	define('PHPOS_VERSION', '1.1');
+	define('PHPOS_BUILD', '2013.10.11');	
+	define('PHPOS_VERSION_NAME', '1.1 beta');
 	define('PHPOS_ONLINE', 'http://phpos.pl/');
 	define('PHPOS_UPDATER', 'http://phpos.pl/updates_server.php');
 	define('PHPOS_GITHUB', 'https://github.com/phpos/phpos/');	
