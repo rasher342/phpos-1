0d2c5ab6416b5291460a302717cd15457130c98d
szczyglis <szczyglis83@gmail.com>
2013-10-14 15:58:37 +0200

    Version update (1.22 beta)

--- web/version.php
+++ web/version.php
@@ -15,9 +15,9 @@
 
 
 /* **************************************** */
-	define('PHPOS_VERSION', '1.21');
+	define('PHPOS_VERSION', '1.22');
 	define('PHPOS_BUILD', '2013.10.14');	
-	define('PHPOS_VERSION_NAME', '1.21 beta');
+	define('PHPOS_VERSION_NAME', '1.22 beta');
 	define('PHPOS_ONLINE', 'http://phpos.pl/');
 	define('PHPOS_UPDATER', 'http://phpos.pl/updates_server.php');
 	define('PHPOS_GITHUB', 'https://github.com/phpos/phpos/');	
