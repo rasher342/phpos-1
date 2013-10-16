78d5854bb85e56ea363a584af9321f2a0bee85cc
szczyglis <szczyglis83@gmail.com>
2013-10-16 19:25:15 +0200

    Version update > 1.26
    1.26
    - Context menus fixed & enchanced
    - Plugins system rebuilded
    - Explorer app rebuilded
    - System core structure changed
    - Lot of fixes in PHP code
    - Login and Installer languages change fix
    - Some icons added
    - Speed up with rendering files

--- web/version.php
+++ web/version.php
@@ -7,7 +7,7 @@
 	(c) 2013 Marcin Szczyglinski
 	szczyglis83@gmail.com
 	GitHUB: https://github.com/phpos/
-	File version: 1.2.5, 2013.10.15
+	File version: 1.2.6, 2013.10.16
  
 **********************************
 */
@@ -15,9 +15,9 @@
 
 
 /* **************************************** */
-	define('PHPOS_VERSION', '1.25');
-	define('PHPOS_BUILD', '2013.10.15');	
-	define('PHPOS_VERSION_NAME', '1.25 beta');
+	define('PHPOS_VERSION', '1.26');
+	define('PHPOS_BUILD', '2013.10.16');	
+	define('PHPOS_VERSION_NAME', '1.26 beta');
 	define('PHPOS_ONLINE', 'http://phpos.pl/');
 	define('PHPOS_UPDATER', 'http://phpos.pl/updates_server.php');
 	define('PHPOS_GITHUB', 'https://github.com/phpos/phpos/');	
